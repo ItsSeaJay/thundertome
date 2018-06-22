4894c70d599e7b7ea8d8a75c4ef64462fa4d97d0
Unknown <b6010572@my.shu.ac.uk>
2018-06-22 13:04:24 +0000

    Addded a boilerplate HTML view

---
+++ application/controllers/Journal.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Journal extends CI_Controller {
+
+	public function __construct()
+	{
+		$assets = array();
+		$assets['helpers'] = array(
+			'url'
+		);
+		$assets['libraries'] = array(
+			'parser'
+		);
+
+		parent::__construct();
+		$this->load->helper('loader');
+		load_assets($assets);
+	}
+
+	public function index()
+	{
+		$parser_data = $this->get_parser_data();
+	}
+
+	public function get_parser_data()
+	{
+		$parser_data = array();
+		$parser_data['base_url'] = base_url();
+		$parser_data['index_page'] = index_page();
+
+		return $parser_data;
+	}
+}
--- application/helpers/loader_helper.php
+++ application/helpers/loader_helper.php
@@ -3,10 +3,47 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 if (!function_exists('load_assets'))
 {
-    function load_assets()
-    {
-        $CI = &get_instance();
+	// Loads in all of the assets needed by a particular class
+	// based on the contents of an associative array
+	function load_assets($assets = array())
+	{
+		$CI = &get_instance();
 
-        // Code...
-    }
+		foreach ($assets as $type => $list)
+		{
+			switch ($type)
+			{
+				case 'configs':
+					foreach ($list as $asset)
+					{
+						$CI->load->config($asset);
+					}
+					break;
+				case 'helpers':
+					foreach ($list as $asset)
+					{
+						$CI->load->helper($asset);
+					}
+					break;
+				case 'languages':
+					foreach ($list as $asset)
+					{
+						$CI->load->language($asset);
+					}
+					break;
+				case 'libraries':
+					foreach ($list as $asset)
+					{
+						$CI->load->library($asset);
+					}
+					break;
+				case 'models':
+					foreach ($list as $asset)
+					{
+						$CI->load->model($asset);
+					}
+					break;
+			}
+		}
+	}
 }
