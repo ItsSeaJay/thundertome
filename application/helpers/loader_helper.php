d8bfcb96396c31087e977083e869c0f425692a03
Unknown <b6010572@my.shu.ac.uk>
2018-06-22 12:04:58 +0000

    Created loader helper

---
+++ application/helpers/loader_helper.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if (!function_exists('load_assets'))
+{
+    function load_assets()
+    {
+        $CI = &get_instance();
+
+        // Code...
+    }
+}
