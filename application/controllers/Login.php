fcba47fdc0b4366c29cb255e63d48bd190cc6f43
Unknown <b6010572@my.shu.ac.uk>
2018-06-26 10:49:02 +0000

    Added login form

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -8,13 +8,14 @@ class Login extends CI_Controller {
 		// Define which kind of assets are needed
 		$assets = array();
 		$assets['models'] = array(
-			// Models
+			'application_model',
+			'author_model'
 		);
 		$assets['helpers'] = array(
-			// Helpers
+			'url'
 		);
 		$assets['libraries'] = array(
-			// Libraries
+			'parser'
 		);
 
 		parent::__construct();
@@ -24,6 +25,32 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
-		echo 'Hello, World!';
+		$parser_data = $this->get_parser_data();
+
+		$this->parser->parse('login/form.html', $parser_data);
+	}
+
+	public function request()
+	{
+		if (isset($_POST['username']) && isset($_POST['password']))
+		{
+			echo 'Good job.';
+		}
+	}
+
+	private function get_parser_data()
+	{
+		$parser_data = array();
+		$parser_data['base_url'] = base_url();
+		$parser_data['index_page'] = index_page();
+		$parser_data['site_url'] = site_url();
+		$parser_data['application_name'] = $this->application_model->get_name();
+
+		return $parser_data;
+	}
+
+	private function get_stylesheets()
+	{
+
 	}
 }
